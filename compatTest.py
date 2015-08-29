9850205922ab46d6e2bba07b55b11ced4a2bc329
Eric Ihli <ericihli@gmail.com>
2015-08-28 22:43:41 -0700

    Create tests for strategy.

--- compatTest.py
+++ compatTest.py
@@ -128,3 +128,13 @@ class arrangementTestCase(unittest.TestCase):
     self.assertIs(group.participants[0], participant)
     self.assertIs(participant.group, group)
 
+class strategyTestCase(unittest.TestCase):
+  def setUp(self):
+    self.arrangement = compat.Arrangement()
+    self.strategy = compat.Strategy(self.arrangement)
+
+  def test_strategy_exists(self):
+    self.assertIsInstance(self.strategy, compat.Strategy)
+
+  def test_strategy_fitness_returns_int(self):
+    self.assertIsInstance(self.strategy.fitness(), int)
